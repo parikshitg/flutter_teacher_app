9395d432b544843b97f9caf90d539528404fd9b0
Parikshit Gothwal <parikshitgothwal14@gmail.com>
2019-12-19 09:13:52 +0530

    date picker

--- lib/screens/students/add_student.dart
+++ lib/screens/students/add_student.dart
@@ -20,8 +20,18 @@ class _AddStudentState extends State<AddStudent> {
   String _fathersPhoneNo = '';
   String _mothersPhoneNo = '';
   bool _gender;
-  String _dob = '';
+  DateTime _dob = DateTime.now();
   String _address = '';
+
+
+  Future _selectDate() async {
+    DateTime picked = await showDatePicker(
+      context: context,
+      initialDate:  DateTime.now(),
+      firstDate: DateTime(2019),
+      lastDate: DateTime.now(),
+    );
+  }
 
 
   @override
@@ -39,128 +49,140 @@ class _AddStudentState extends State<AddStudent> {
           onPressed: () => Navigator.pop(context),
         ),
       ),
-      body: ListView(
-        children: <Widget>[
-          Padding(
-            padding: EdgeInsets.all(30.0),
-            child: Form(
-              key: _formKey,
-              child: Column(
-                children: <Widget>[
-                  CircleAvatar(
-                    radius: 60.0,
-                    backgroundImage: AssetImage('assets/images/avatar.jpg'),
-                  ),
-                  TextFormField(
-                    initialValue: _name,
-                    /*style: TextStyle(
-                      fontSize: 18.0
-                    ),*/
-                    decoration: InputDecoration(
-                      icon: Icon(Icons.person,),
-                      labelText: 'Name',
+      body: GestureDetector(
+        onTap: () => FocusScope.of(context)
+            .unfocus(),
+              child: ListView(
+          children: <Widget>[
+            Padding(
+              padding: EdgeInsets.all(30.0),
+              child: Form(
+                key: _formKey,
+                child: Column(
+                  children: <Widget>[
+                    CircleAvatar(
+                      radius: 60.0,
+                      backgroundImage: AssetImage('assets/images/avatar.jpg'),
                     ),
-                  ),
-                  TextFormField(
-                    initialValue: _rollNo,
-                    /*style: TextStyle(
-                      fontSize: 18.0
-                    ),*/
-                    decoration: InputDecoration(
-                      icon: Icon(Icons.confirmation_number,),
-                      labelText: 'Roll No',
+                    TextFormField(
+                      initialValue: _name,
+                      /*style: TextStyle(
+                        fontSize: 18.0
+                      ),*/
+                      decoration: InputDecoration(
+                        icon: Icon(Icons.person,),
+                        labelText: 'Name',
+                      ),
                     ),
-                  ),
-                  TextFormField(
-                    initialValue: _bloodGroup,
-                    /*style: TextStyle(
-                      fontSize: 18.0
-                    ),*/
-                    decoration: InputDecoration(
-                      icon: Icon(Icons.person,),
-                      labelText: 'Blood Group',
+                    SizedBox(height: 10.0,),
+                    TextFormField(
+                      initialValue: _rollNo,
+                      /*style: TextStyle(
+                        fontSize: 18.0
+                      ),*/
+                      decoration: InputDecoration(
+                        icon: Icon(Icons.confirmation_number,),
+                        labelText: 'Roll No',
+                      ),
                     ),
-                  ),
-                  TextFormField(
-                    initialValue: _name,
-                    /*style: TextStyle(
-                      fontSize: 18.0
-                    ),*/
-                    decoration: InputDecoration(
-                      icon: Icon(Icons.person,),
-                      labelText: 'Fathers Name',
+                    SizedBox(height: 10.0,),
+                    TextFormField(
+                      initialValue: _bloodGroup,
+                      /*style: TextStyle(
+                        fontSize: 18.0
+                      ),*/
+                      decoration: InputDecoration(
+                        icon: Icon(Icons.person,),
+                        labelText: 'Blood Group',
+                      ),
                     ),
-                  ),
-                  TextFormField(
-                    initialValue: _name,
-                    /*style: TextStyle(
-                      fontSize: 18.0
-                    ),*/
-                    decoration: InputDecoration(
-                      icon: Icon(Icons.person,),
-                      labelText: 'Fathers Phone No',
+                    SizedBox(height: 10.0,),
+                    TextFormField(
+                      initialValue: _fathersName,
+                      /*style: TextStyle(
+                        fontSize: 18.0
+                      ),*/
+                      decoration: InputDecoration(
+                        icon: Icon(Icons.person,),
+                        labelText: 'Fathers Name',
+                      ),
                     ),
-                  ),
-                  TextFormField(
-                    initialValue: _name,
-                    /*style: TextStyle(
-                      fontSize: 18.0
-                    ),*/
-                    decoration: InputDecoration(
-                      icon: Icon(Icons.person,),
-                      labelText: 'Mothers Name',
+                    SizedBox(height: 10.0,),
+                    TextFormField(
+                      initialValue: _fathersPhoneNo,
+                      /*style: TextStyle(
+                        fontSize: 18.0
+                      ),*/
+                      decoration: InputDecoration(
+                        icon: Icon(Icons.person,),
+                        labelText: 'Fathers Phone No',
+                      ),
                     ),
-                  ),
-                  TextFormField(
-                    initialValue: _name,
-                    /*style: TextStyle(
-                      fontSize: 18.0
-                    ),*/
-                    decoration: InputDecoration(
-                      icon: Icon(Icons.person,),
-                      labelText: 'Mothers Phone No',
+                    SizedBox(height: 10.0,),
+                    TextFormField(
+                      initialValue: _mothersName,
+                      /*style: TextStyle(
+                        fontSize: 18.0
+                      ),*/
+                      decoration: InputDecoration(
+                        icon: Icon(Icons.person,),
+                        labelText: 'Mothers Name',
+                      ),
                     ),
-                  ),
-                  TextFormField(
-                    initialValue: _name,
-                    /*style: TextStyle(
-                      fontSize: 18.0
-                    ),*/
-                    decoration: InputDecoration(
-                      icon: Icon(Icons.person,),
-                      labelText: 'Gender',
+                    SizedBox(height: 10.0,),
+                    TextFormField(
+                      initialValue: _mothersPhoneNo,
+                      /*style: TextStyle(
+                        fontSize: 18.0
+                      ),*/
+                      decoration: InputDecoration(
+                        icon: Icon(Icons.person,),
+                        labelText: 'Mothers Phone No',
+                      ),
                     ),
-                  ),
-                  TextFormField(
-                    initialValue: _name,
-                    /*style: TextStyle(
-                      fontSize: 18.0
-                    ),*/
-                    decoration: InputDecoration(
-                      icon: Icon(Icons.person,),
-                      labelText: 'DOB',
+                    SizedBox(height: 10.0,),
+                    TextFormField(
+                      /*style: TextStyle(
+                        fontSize: 18.0
+                      ),*/
+                      decoration: InputDecoration(
+                        icon: Icon(Icons.person,),
+                        labelText: 'Gender',
+                      ),
                     ),
-                  ),
-                  TextFormField(
-                    initialValue: _name,
-                    /*style: TextStyle(
-                      fontSize: 18.0
-                    ),*/
-                    decoration: InputDecoration(
-                      icon: Icon(Icons.person,),
-                      labelText: 'Address',
+                    SizedBox(height: 10.0,),
+                    TextFormField(
+                      onTap: _selectDate,
+                      /*style: TextStyle(
+                        fontSize: 18.0
+                      ),*/
+                      decoration: InputDecoration(
+                        icon: Icon(Icons.person,),
+                        labelText: 'DOB',
+                      ),
                     ),
-                  ),
-                ],
+                    SizedBox(height: 10.0,),
+                    TextFormField(
+                      initialValue: _address,
+                      /*style: TextStyle(
+                        fontSize: 18.0
+                      ),*/
+                      decoration: InputDecoration(
+                        icon: Icon(Icons.person,),
+                        labelText: 'Address',
+                      ),
+                    ),
+                  ],
+                ),
               ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
       bottomNavigationBar: Padding(
         padding: EdgeInsets.all(0.0),
         child: RaisedButton(
-          onPressed: () {},
+          onPressed: () {print('save clicked');},
           color: Colors.black,
           textColor: Colors.white,
           child: Text('Save'),
