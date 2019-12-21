ca77c2452c3b9c0ace65a4aba53b1d09ab98585b
Parikshit Gothwal <parikshitgothwal14@gmail.com>
2019-12-21 09:42:01 +0530

    student list card

--- lib/screens/students/students_homescreen.dart
+++ lib/screens/students/students_homescreen.dart
@@ -3,7 +3,6 @@ import 'package:flutter/material.dart';
 import './add_student.dart';
 
 class StudentsHomescreen extends StatefulWidget {
-
   static final String id = 'students_homescreen';
 
   @override
@@ -15,7 +14,10 @@ class _StudentsHomescreenState extends State<StudentsHomescreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Class - 5 C", style: TextStyle(color: Colors.black),),
+        title: Text(
+          "Class - 5 C",
+          style: TextStyle(color: Colors.black),
+        ),
         backgroundColor: Colors.white,
         leading: IconButton(
           icon: Icon(Icons.arrow_back),
@@ -30,7 +32,53 @@ class _StudentsHomescreenState extends State<StudentsHomescreen> {
           ),
         ],
       ),
-      body: Center(child: Text("STUDENTS"),),
+      body: Column(
+        children: <Widget>[
+          Card(
+            elevation: 5,
+            margin: EdgeInsets.symmetric(
+              vertical: 8,
+              horizontal: 5,
+            ),
+            child: ListTile(
+              leading: CircleAvatar(
+                radius: 30,
+                child: Padding(
+                  padding: EdgeInsets.all(6),
+                ),
+                backgroundColor: Colors.grey,
+              ),
+              title: Text("Student Name"),
+              subtitle: Text(
+                "s/o Students Fathers Name",
+              ),
+              trailing: Row(
+                mainAxisSize: MainAxisSize.min,
+                children: <Widget>[
+                  IconButton(
+                    icon: Icon(
+                      Icons.phone,
+                      color: Colors.black54,
+                    ),
+                    onPressed: () {
+                      print('call button clicked');
+                    },
+                  ),
+                  IconButton(
+                    icon: Icon(
+                      Icons.mail,
+                      color: Colors.black54,
+                    ),
+                    onPressed: () {
+                      print('sms button clicked');
+                    },
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
