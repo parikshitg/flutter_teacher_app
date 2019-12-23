8e1ec2ecee0b4b4d7a739cfcaa38e3338198760c
Parikshit Gothwal <parikshitgothwal14@gmail.com>
2019-12-23 20:36:27 +0530

    attendance list

--- lib/models/student_model.dart
+++ lib/models/student_model.dart
@@ -26,3 +26,87 @@ class Student {
       this.address,
       this.profileUrl});
 }
+
+final List<Student> students = [
+    Student(
+      name: 'John Cena',
+      rollNo: '1',
+      fathersName: 's/o John Cena Sr',
+    ),
+    Student(
+      name: 'Randy Orton',
+      rollNo: '2',
+      fathersName: 's/o Randy Keith Sr',
+    ),
+    Student(
+      name: 'Triple HHH',
+      rollNo: '3',
+      fathersName: 's/o Vince McMahon Sr',
+    ),
+    Student(
+      name: 'Shawn Michaels',
+      rollNo: '4',
+      fathersName: 's/o Bret Hart Sr',
+    ),
+    Student(
+      name: 'Batista',
+      rollNo: '5',
+      fathersName: 's/o Dave Bautista Sr',
+    ),
+    Student(
+      name: 'Rey Mysterio',
+      rollNo: '6',
+      fathersName: 's/o Rey Mysterio Sr',
+    ),
+    Student(
+      name: 'Seth Rollins',
+      rollNo: '7',
+      fathersName: 's/o Rollins Seth Sr',
+    ),
+    Student(
+      name: 'Undertaker',
+      rollNo: '8',
+      fathersName: 's/o Mark Calaway Sr',
+    ),
+    Student(
+      name: 'Kane',
+      rollNo: '9',
+      fathersName: 's/o Mark Calaway Sr',
+    ),
+    Student(
+      name: 'Stone Cold',
+      rollNo: '10',
+      fathersName: 's/o Steve Austin Sr',
+    ),
+    Student(
+      name: 'Roman Reigns',
+      rollNo: '11',
+      fathersName: 's/o Roman Reigns Sr',
+    ),
+    Student(
+      name: 'Dean Ambrose',
+      rollNo: '12',
+      fathersName: 's/o Jon Moxley Sr',
+    ),
+    Student(
+      name: 'Mike Mizanin',
+      rollNo: '13',
+      fathersName: 's/o The Miz Sr',
+    ),
+    Student(
+      name: 'Dolph Ziggler',
+      rollNo: '14',
+      fathersName: 's/o Dolph Ziggler Sr',
+    ),
+    Student(
+      name: 'Big Show',
+      rollNo: '15',
+      fathersName: 's/o Mark Henry Sr',
+    ),
+    Student(
+      name: 'The Rock',
+      rollNo: '16',
+      fathersName: 's/o Dywane Johnson Sr',
+    ),
+  ];
+
--- lib/screens/attendance/attendance_homescreen.dart
+++ lib/screens/attendance/attendance_homescreen.dart
@@ -1,7 +1,9 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class AttendanceHomescreen extends StatefulWidget {
+import '../../models/student_model.dart';
 
+class AttendanceHomescreen extends StatefulWidget {
   static final String id = "attendance_homeScreen";
 
   @override
@@ -9,10 +11,74 @@ class AttendanceHomescreen extends StatefulWidget {
 }
 
 class _AttendanceHomescreenState extends State<AttendanceHomescreen> {
+  bool checkBoxValue = false;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(child: Text("Attendance"),),
+      appBar: AppBar(
+        title: Text(
+          'Attendance',
+          style: TextStyle(color: Colors.black),
+        ),
+        backgroundColor: Colors.white,
+        leading: IconButton(
+          icon: Icon(
+            Icons.arrow_back,
+            color: Colors.black,
+          ),
+          onPressed: () => Navigator.pop(context),
+        ),
+        actions: <Widget>[
+          FlatButton(
+            child: Text('save'),
+            onPressed: () {
+              print('Save CLicked');
+            },
+          ),
+        ],
+        /*bottom: TabBar(tabs: <Widget>[
+          TabController(),
+          Text('Date Picker'),
+        ],),*/
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: students.map(
+            (tx) {
+              return Container(
+                decoration: BoxDecoration(
+                  border: Border(
+                    bottom: BorderSide(
+                      color: Colors.grey,
+                    ),
+                  ),
+                ),
+                child: ListTile(
+                  leading: CircleAvatar(
+                    radius: 30,
+                    child: Padding(
+                      padding: EdgeInsets.all(6),
+                    ),
+                    backgroundColor: Colors.grey,
+                  ),
+                  title: Text('Student Name'),
+                  subtitle: Text('Roll No. 1'),
+                  trailing: Checkbox(
+                    value: checkBoxValue,
+                    onChanged: (bool v) {
+                      setState(() {
+                        checkBoxValue = v;
+                        print(v);
+                      });
+                    },
+                  ),
+                ),
+              );
+            },
+          ).toList(),
+        ),
+      ),
     );
   }
 }
