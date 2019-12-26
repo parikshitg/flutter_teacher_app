3a3497e0d597e6ee810d7e3ff36dde8d23ad4b61
Parikshit Gothwal <parikshitgothwal14@gmail.com>
2019-12-26 17:56:09 +0530

    tab bar for schedule

--- lib/screens/schdule/schedule_homescreen.dart
+++ lib/screens/schdule/schedule_homescreen.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 
 class ScheduleHomeScreen extends StatefulWidget {
-
   static final String id = 'schedule_homescreen';
 
   @override
@@ -11,8 +10,67 @@ class ScheduleHomeScreen extends StatefulWidget {
 class _ScheduleHomeScreenState extends State<ScheduleHomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(child: Text("Schedule Home Screen"),),
+    return DefaultTabController(
+      length: 6,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text(
+            'Schedule',
+            style: TextStyle(color: Colors.black),
+          ),
+          backgroundColor: Colors.white,
+          leading: IconButton(
+            icon: Icon(Icons.arrow_back_ios),
+            color: Colors.black,
+            onPressed: () => Navigator.pop(context),
+          ),
+          actions: <Widget>[
+            IconButton(
+              icon: Icon(Icons.add),
+              color: Colors.black,
+              onPressed: () => print('add schedule'),
+            )
+          ],
+          bottom: TabBar(
+            tabs: <Widget>[
+              Text(
+                'Mon',
+                style: TextStyle(color: Colors.black),
+              ),
+              Text(
+                'Tue',
+                style: TextStyle(color: Colors.black),
+              ),
+              Text(
+                'Wed',
+                style: TextStyle(color: Colors.black),
+              ),
+              Text(
+                'Thu',
+                style: TextStyle(color: Colors.black),
+              ),
+              Text(
+                'Fri',
+                style: TextStyle(color: Colors.black),
+              ),
+              Text(
+                'Sat',
+                style: TextStyle(color: Colors.black),
+              ),
+            ],
+          ),
+        ),
+        body: TabBarView(
+          children: <Widget>[
+            Text('Monday'),
+            Text('Tuesday'),
+            Text('Wenesday'),
+            Text('Thursday'),
+            Text('Friday'),
+            Text('Saturday'),
+          ],
+        ),
+      ),
     );
   }
 }
